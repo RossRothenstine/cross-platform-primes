48fdccbb66daec495c31a53527774b510f589b5d
Ross Rothenstine <rossrothenstine@gmail.com>
2023-09-16 17:51:01 -0700

    Docs up

--- core/src/Primes.h
+++ core/src/Primes.h
@@ -10,8 +10,8 @@ namespace primes {
 // If there was ever a way to be given invalid input, returning std::optional might be the play with nullopt.
 [[nodiscard]] std::vector<int> ParseNumbers(const std::string_view& input) noexcept;
 
-// Given an array of prime indices (e.g., 1, 2, 3), return an array of those prime numbers.
-// E.g., 1, 3, 5.
+// Given an array of prime indices (e.g., 1, 2, 3), put the corresponding prime index into the array at
+// the same location., e.g., (2, 3, 5).
 void GetPrimes(std::vector<int>& indicies) noexcept;
 
 // Return the given Prime at index. This isn't necessary to be exposed via header, but for the sake of testing
